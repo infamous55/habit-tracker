797ab8bf6b8ab7ea09cce252e54ba5b251ef5b84
infamous55 <infamouslegend555@gmail.com>
2023-10-24 23:27:49 +0300

    added mongodb success test

---
+++ internal/mongodb/success_test.go
@@ -0,0 +1,165 @@
+package mongodb
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/integration/mtest"
+
+	"github.com/infamous55/habit-tracker/internal/models"
+)
+
+func TestGetSuccessByID(t *testing.T) {
+	mt := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
+	defer mt.Close()
+
+	mt.Run("success", func(mt *mtest.T) {
+		dbw := DatabaseWrapper{mt.DB}
+
+		successID := primitive.NewObjectID()
+		habitID := primitive.NewObjectID()
+
+		date := time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC)
+
+		expectedSuccess := models.Success{
+			ID:      successID,
+			Date:    date,
+			HabitID: habitID,
+		}
+
+		mt.AddMockResponses(
+			mtest.CreateCursorResponse(
+				1,
+				"collections.successes",
+				mtest.FirstBatch,
+				bson.D{
+					{Key: "_id", Value: successID},
+					{Key: "date", Value: date},
+					{Key: "habit_id", Value: habitID},
+				},
+			),
+		)
+
+		response, err := dbw.GetSuccessByID(successID)
+		assert.Nil(t, err)
+		assert.Equal(t, &expectedSuccess, response)
+	})
+}
+
+func TestGetSuccessByHabitID(t *testing.T) {
+	mt := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
+	defer mt.Close()
+
+	mt.Run("success", func(mt *mtest.T) {
+		dbw := DatabaseWrapper{mt.DB}
+
+		habitID := primitive.NewObjectID()
+
+		firstSuccessID := primitive.NewObjectID()
+		secondSuccessID := primitive.NewObjectID()
+
+		date := time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC)
+
+		firstCursor := mtest.CreateCursorResponse(
+			1,
+			"collections.successes",
+			mtest.FirstBatch,
+			bson.D{
+				{Key: "_id", Value: firstSuccessID},
+				{Key: "date", Value: date},
+				{Key: "habit_id", Value: habitID},
+			},
+		)
+		secondCursor := mtest.CreateCursorResponse(
+			1,
+			"collections.successes",
+			mtest.NextBatch,
+			bson.D{
+				{Key: "_id", Value: secondSuccessID},
+				{Key: "date", Value: date},
+				{Key: "habit_id", Value: habitID},
+			},
+		)
+		killCursor := mtest.CreateCursorResponse(
+			1,
+			"collections.successes",
+			mtest.NextBatch,
+		)
+		mt.AddMockResponses(
+			firstCursor, secondCursor, killCursor,
+		)
+
+		response, err := dbw.GetSuccessesByHabitID(habitID)
+		assert.Nil(t, err)
+		assert.Equal(t, []*models.Success{
+			{
+				ID:      firstSuccessID,
+				Date:    date,
+				HabitID: habitID,
+			},
+			{
+				ID:      secondSuccessID,
+				Date:    date,
+				HabitID: habitID,
+			},
+		}, response)
+	})
+}
+
+func TestCreateSuccess(t *testing.T) {
+	mt := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
+	defer mt.Close()
+
+	mt.Run("success", func(mt *mtest.T) {
+		dbw := DatabaseWrapper{mt.DB}
+
+		date := time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC)
+		habitID := primitive.NewObjectID()
+		data := models.SuccessCreate{
+			Date:    date,
+			HabitID: habitID,
+		}
+
+		mt.AddMockResponses(mtest.CreateSuccessResponse())
+
+		response, err := dbw.CreateSuccess(data)
+		assert.Nil(t, err)
+		assert.NotEqual(t, response.ID.Hex(), "")
+	})
+}
+
+func TestDeleteSuccessByID(t *testing.T) {
+	mt := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
+	defer mt.Close()
+
+	mt.Run("success", func(mt *mtest.T) {
+		dbw := DatabaseWrapper{mt.DB}
+
+		successID := primitive.NewObjectID()
+		habitID := primitive.NewObjectID()
+		date := time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC)
+		expectedSuccess := models.Success{
+			ID:      successID,
+			Date:    date,
+			HabitID: habitID,
+		}
+
+		mt.AddMockResponses(
+			bson.D{
+				{Key: "ok", Value: 1},
+				{Key: "value", Value: bson.D{
+					{Key: "_id", Value: successID},
+					{Key: "date", Value: date},
+					{Key: "habit_id", Value: habitID},
+				}},
+			},
+		)
+
+		response, err := dbw.DeleteSuccessByID(successID)
+		assert.Nil(t, err)
+		assert.Equal(t, &expectedSuccess, response)
+	})
+}
